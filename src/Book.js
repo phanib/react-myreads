0fdf96409c0cc6acd2ddadbb4d78bdbfd68a8676
Phaneendra B <phani.brahma@gmail.com>
2018-05-09 16:56:12 -0500

    Refactored Book to functional component

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Book extends Component {
+function Book({book, handleChange}) {
   
-  handleUpdate = (e) => {
-    this.props.handleChange(this.props.book, e)
-  }
-
-  render() {
-    const { imageLinks, shelf, title, authors} = this.props.book
-
-    return  (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${imageLinks && imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select defaultValue={shelf} onChange={this.handleUpdate}>
-              <option value="movedTo" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  const { shelf, title, authors} = book
+  const bookThumbnail = book.imageLinks ? book.imageLinks.thumbnail : "http://via.placeholder.com/128x193?text=No%20Cover"
+  
+  return  (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${bookThumbnail})` }}></div>
+        <div className="book-shelf-changer">
+          <select defaultValue={shelf} onChange={e => handleChange(book, e)}>
+            <option value="movedTo" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{authors && authors.map(a => `${a} `)}</div>
       </div>
-    )
-  }
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors && authors.map(a => `${a} `)}</div>
+    </div>
+  )
 }
 
 export default Book
